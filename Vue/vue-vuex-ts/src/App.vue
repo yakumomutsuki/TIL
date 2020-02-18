54ef3c0d449f4c177123acdfd62c7392234bd1a8
yakumomutsuki <sakuraapp1988@gmail.com>
2020-02-19 00:54:43 +0900

    Add Bootstrap, BootstrapVue

--- Vue/vue-vuex-ts/src/App.vue
+++ Vue/vue-vuex-ts/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <button class="btn btn-primary">This is Bootstrap styled button</button>
   </div>
 </template>
 
--- Vue/vue-vuex-ts/src/main.ts
+++ Vue/vue-vuex-ts/src/main.ts
@@ -1,10 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+import BootstrapVue from "bootstrap-vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue)
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 new Vue({
   router,
